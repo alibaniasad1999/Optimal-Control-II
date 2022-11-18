2f28c9d19a1338b09f63ded7f03b5726acb9de05
alibaniasad1999 <alibaniasad1999@yahoo.com>
2022-11-18 22:32:46 +0330

    add fig

--- HW/HW1/Code/Bonus/Bonus.m
+++ HW/HW1/Code/Bonus/Bonus.m
@@ -6,6 +6,7 @@ in_circule = 0;
 max_iteration = 100000;
 pi_estimation = zeros(1, max_iteration);
 i = 1;
+var_ter = 10^-10;
 while true
     if i > max_iteration
         break
@@ -21,17 +22,22 @@ while true
         print('../../Figure/Bonus/mont_1000', '-depsc');
     end
     if i == 10000
+        plot(linspace(0, 1, 1000), sqrt(1-(linspace(0, 1, 1000)).^2), 'k',...
+    'LineWidth',3)
         print('../../Figure/Bonus/mont_10000', '-depsc');
     end
     pi_estimation(i) = in_circule * 4 / i;
-    if i > 1000
-        if abs(pi_estimation(i) - pi_estimation(i-1)) < 0.00001
+    if i > 100
+        if var(pi_estimation(i-10:i)) < var_ter
             break;
         end
     end
     i = i + 1;
 end
-
+plot(linspace(0, 1, 1000), sqrt(1-(linspace(0, 1, 1000)).^2), 'k',...
+    'LineWidth',3)
+print(append('../../Figure/Bonus/mont', string(var_ter)), '-depsc');
 pi_mont_final = in_circule * 4 / i;
+save(append('var', string(var_ter*10^10), 'data'), "pi_mont_final")
 
 
